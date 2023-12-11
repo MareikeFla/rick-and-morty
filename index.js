f410e0cfa9d2c709ab3f448fd704984b8cf2348c
Philipp Frentzen <philipp.frentzen@gmail.com>
2023-12-11 11:41:18 +0100

    update show max page

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 // States
-const maxPage = 1;
+let maxPage = 1;
 const page = 1;
 const searchQuery = "";
 
@@ -18,6 +18,7 @@ export async function fetchCharacters() {
   const url = "https://rickandmortyapi.com/api/character";
   const response = await fetch(url);
   const data = await response.json();
+  pagination.textContent = `1 / ${data.info.pages}`;
   console.log(data);
   return data.results;
 }
